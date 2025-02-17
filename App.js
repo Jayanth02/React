77dd615d99199f974b2781248af666b22f410368
Jayanth Kumar <kodam.jayanth002@gmail.com>
2025-02-17 15:11:44 +0530

    React part 1

--- App.js
+++ App.js
@@ -1,17 +1,7 @@
-/*
-<div id="parent">
-    <div id="class">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id="class2">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div>
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-*/
 
 const parent = React.createElement("div",{id:"parent"},
                 [React.createElement("div",{id:"class"},
@@ -25,6 +15,5 @@ const parent = React.createElement("div",{id:"parent"},
 
 console.log(parent);
 
-const heading = React.createElement("h1",{id: "heading"},"Hellow world from React!!");
-const root = ReactDOM.createRoot(document.getElementById("header"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #React tutorial
+
+#parcel
+-Dev Build
+-Local server
+-HMR - Hot Module Replacement
