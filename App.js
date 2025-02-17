42622970ecbe72af778c24e2ac0948f4055efaf0
Jayanth Kumar <kodam.jayanth002@gmail.com>
2025-02-17 10:54:26 +0530

    React part 1

---
+++ App.js
@@ -0,0 +1,30 @@
+/*
+<div id="parent">
+    <div id="class">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+    <div id="class2">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+</div>
+
+
+*/
+
+const parent = React.createElement("div",{id:"parent"},
+                [React.createElement("div",{id:"class"},
+                [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
+                ),
+                React.createElement("div",{id:"class2"},
+                [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
+                )]
+);
+
+
+console.log(parent);
+
+const heading = React.createElement("h1",{id: "heading"},"Hellow world from React!!");
+const root = ReactDOM.createRoot(document.getElementById("header"));
+root.render(parent);
